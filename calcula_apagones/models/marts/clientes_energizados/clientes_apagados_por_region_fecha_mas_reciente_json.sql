75e5c5a8530ff3a11bb1c131f6fbde849886629a
José M. Zavala González <jose.m.zavala.gonzalez@gmail.com>
2024-02-18 21:08:24 -0400

    Add hora mas reciente

--- calcula_apagones/models/marts/clientes_energizados/clientes_apagados_por_region_fecha_mas_reciente_json.sql
+++ calcula_apagones/models/marts/clientes_energizados/clientes_apagados_por_region_fecha_mas_reciente_json.sql
@@ -5,10 +5,20 @@ select
 from {{ ref('clientes_apagados_por_region_por_dia') }}
 ),
 
+hora_mas_reciente as (
+select
+  date_trunc('day', marca_hora_presentada) as marca_fecha_presentada,
+  max(marca_hora_presentada) as marca_hora_presentada_mas_reciente
+from {{ ref('clientes_energizados_por_region') }}
+group by marca_fecha_presentada
+),
+
 clientes_apagados_por_region_fecha_mas_reciente as (
     select *
         exclude (marca_fecha_presentada_mas_reciente)
     from fecha_mas_reciente
+    full join hora_mas_reciente
+    using (marca_fecha_presentada)
     where marca_fecha_presentada = marca_fecha_presentada_mas_reciente
 ),
 
@@ -20,6 +30,7 @@ clientes_apagados_totales_fecha_mas_reciente as (
     sum(capr.total_clientes) as total_clientes,
     sum(capr.total_clientes_sin_servicio) / sum(capr.total_clientes) * 100 as porcentaje_clientes_sin_servicio,
     first(capr.marca_fecha_presentada) as marca_fecha_presentada,
+    first(capr.marca_hora_presentada_mas_reciente) as marca_hora_presentada_mas_reciente,
   from clientes_apagados_por_region_fecha_mas_reciente as capr
 ),
 
@@ -37,6 +48,7 @@ structed_totals_data as (
     list(clientes_apagados_por_region_fecha_mas_reciente) as numeros_regiones,
     first(clientes_apagados_totales_fecha_mas_reciente) as numeros_totales,
     numeros_totales['marca_fecha_presentada'] as marca_fecha_presentada,
+    numeros_totales['marca_hora_presentada_mas_reciente'] as marca_hora_presentada_mas_reciente,
  from structed_regions_data
  full join
  structed_totals_data
